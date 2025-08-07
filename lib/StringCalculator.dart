9e3e1ed4d195785baf2cf91958e85217aeba963c
nidhibizzmanweb <nidhi.bizzmanweb@gmail.com>
2025-08-07 18:35:52 +0530

    feat: support single custom delimiter

--- lib/StringCalculator.dart
+++ lib/StringCalculator.dart
@@ -3,22 +3,28 @@ class StringCalculator{
   int add(String numbers) {
     if (numbers.trim().isEmpty) return 0;
 
-    String delimiter = ',|\n';
+    String delimiterPattern = r',|\n';
     String numberString = numbers;
 
     if (numbers.startsWith('//')) {
-      final parts = numbers.split('\n');
-      delimiter = RegExp.escape(parts[0].substring(2));
-      numberString = parts[1];
+      final delimiterEndIndex = numbers.indexOf('\n');
+      final delimiterPart = numbers.substring(2, delimiterEndIndex);
+      numberString = numbers.substring(delimiterEndIndex + 1);
+
+      if (delimiterPart.startsWith('[')) {
+        final matches = RegExp(r'\[([^\]]+)\]').allMatches(delimiterPart);
+        final delimiters = matches.map((m) => RegExp.escape(m.group(1)!)).join('|');
+        delimiterPattern = delimiters;
+      } else {
+        delimiterPattern = RegExp.escape(delimiterPart);
+      }
     }
 
     final numberList = numberString
-        .split(RegExp(delimiter))
-        .map(int.parse)
+        .split(RegExp(delimiterPattern))
+        .map((e) => int.parse(e))
         .toList();
 
     return numberList.where((n) => n <= 1000).reduce((a, b) => a + b);
   }
-
-
 }
--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -23,4 +23,9 @@ void main() {
   test('supports custom single-character delimiter', () {
     expect(calculator.add('//;\n1;2'), 3);
   });
+
+  test('should support multiple and multi-character custom delimiters', () {
+    final calculator = StringCalculator();
+    expect(calculator.add('//[***][%]\n1***2%3'), 6);
+  });
 }
