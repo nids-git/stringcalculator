5b18aa12516dff4498f3ebd82e3566a5e285dba1
nidhibizzmanweb <nidhi.bizzmanweb@gmail.com>
2025-08-07 17:49:46 +0530

    test: single number input returns the same number

--- lib/StringCalculator.dart
+++ lib/StringCalculator.dart
@@ -2,7 +2,10 @@
 class StringCalculator{
   int add(String numbers) {
     if (numbers.trim().isEmpty) return 0;
-    return 0;
+
+    final nums = numbers.split(',').map((e) => int.parse(e)).toList();
+    return nums.reduce((a, b) => a + b);
   }
+
 
 }
--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -7,4 +7,8 @@ void main() {
   test('returns 0 for empty string', () {
     expect(calculator.add(''), 0);
   });
+
+  test('returns number when single number is given', () {
+    expect(calculator.add('5'), 5);
+  });
 }
